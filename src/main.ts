1a94fddcff8ae8043ccb3653e8c87bcfb7b4593b
barham <baranali6688@gmail.com>
2024-11-10 21:13:51 +0300

    Dynamic Port

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import process from "node:process"
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3000);
+  await app.listen(process.env.PORT);
 }
 
 bootstrap();
